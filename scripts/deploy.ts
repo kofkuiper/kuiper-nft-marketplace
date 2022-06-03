7606d5d1453abf46dcf57d2b07b8a4beafb203f5
kofkuiper <kofkuiper@gmail.com>
2022-06-03 17:13:06 +0700

    Add deploy script.

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,30 +1,27 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { BigNumber } from 'ethers';
+import { ethers } from 'hardhat';
+import { KuiperNFTFactory__factory, KuiperNFTMarketplace__factory } from '../typechain';
+
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const signers = await ethers.getSigners();
+  const KuiNFTFactory = new KuiperNFTFactory__factory(signers[0]);
+  const kuiperNFTFactory = await KuiNFTFactory.deploy();
+  await kuiperNFTFactory.deployed();
+  console.log('KuiperNFTFactory deployed to: ', kuiperNFTFactory.address);
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  const KuiperNFTMarketplace = new KuiperNFTMarketplace__factory(signers[0]);
+  const platformFee = BigNumber.from(10); // 10%
+  const feeRecipient = signers[0].address;
+  const kuiperNFTMarketplace =  await KuiperNFTMarketplace.deploy(platformFee, feeRecipient, kuiperNFTFactory.address);
+  await kuiperNFTMarketplace.deployed();
+  console.log('KuiperNFTMarketplace deployed to: ', kuiperNFTMarketplace.address);
+  
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
-});
+
+})
