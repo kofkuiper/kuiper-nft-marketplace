463b583752d73c9bab96f29d117dcbb2883b7abf
kofkuiper <kofkuiper@gmail.com>
2022-06-03 21:20:55 +0700

    Add Token Smart Contract.

---
+++ contracts/test/Token.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+// Author: kofkuiper
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20 {
+    constructor(string memory _name, string memory _symbol)
+        ERC20(_name, _symbol)
+    {
+        _mint(msg.sender, 1_000_000_000 ether);
+    }
+}
