c85a8cdcca0862aef9f80b51398a912328182e98
kofkuiper <kofkuiper@gmail.com>
2022-06-03 17:06:05 +0700

    Enabled optimizer.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.4',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+    ]
+  },
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
